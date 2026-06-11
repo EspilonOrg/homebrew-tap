7603cdec1fb058508c10801306652c94ef5e8ec7
Eun0us <eun0us@proton.me>
2026-06-11 23:38:46 +0200

    emon 0.1.1

--- Formula/emon.rb
+++ Formula/emon.rb
@@ -1,19 +1,16 @@
 class Emon < Formula
   desc "Universal serial monitor for embedded devices"
   homepage "https://github.com/EspilonOrg/emon"
-  url "https://github.com/EspilonOrg/emon/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "b5abea543f7970eb662228cd13fa533b6db9cfcef43348ff89424446f4253bcc"
+  url "https://github.com/EspilonOrg/emon/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "f1ae820b4ecd3122e82c497ea3ba1852a2f336b79ffaa545d6912a435fcf96a9"
   license "Apache-2.0"
-  version "0.1.0"
+  version "0.1.1"
 
   def install
-    system "make"
-    bin.install "espilon-monitor" => "emon"
-    man1.install "docs/emon.1"
-    (share/"emon/patterns").install Dir["patterns/*"]
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/emon --version")
+    assert_match "0.1.1", shell_output("#{bin}/emon --version")
   end
 end
